cae125b3004558dae0d38619c8ef5f181a856ab6
leila <delcampo.leila@gmail.com>
2020-06-08 11:00:43 -0300

    contador de productos

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -15,4 +15,15 @@ class ProductModel extends BaseModel
         $this->tableName = 'products';
     }
 
+
+    function countProducts(){
+        $response = $this->getDb()->fetch_row('SELECT COUNT(id) AS total FROM '.$this->tableName.' WHERE state = ?',"alive");
+
+        if($response['total']!=null){
+            return $response;
+        }else{
+            $response['total']=0;
+            return $response;
+        }
+    }
 }
